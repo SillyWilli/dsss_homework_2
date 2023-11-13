0d55a8fb1def47b4b9e788a9dcf3351f73bbba58
unknown <david.weislein@gmail.com>
2023-11-13 23:28:05 +0100

    next sixth commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dsss_homework_2',
+    version='1.0',
+    author='DavidW',
+    packages=find_packages(),
+    install_requires=["numpy"],
+    url="https://github.com/SillyWilli/dsss_homework_2.git"
+)
